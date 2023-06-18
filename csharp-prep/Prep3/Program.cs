978725e82e553babe5da3cde7a19d6e3fbf694a0
Kate Paletta <kate.f.knox@gmail.com>
2023-06-17 18:53:09 -0600

    generate and print the random number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1,101);
+
+        Console.Write($"The magic number is: {magicNum}");
     }
 }
