fa4e95fc2fc748f4c760d55f123c54e14ab07b0f
Kate Paletta <kate.f.knox@gmail.com>
2023-06-17 18:28:59 -0600

    take user input

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string answer = Console.ReadLine();
+        int gradePercent = int.Parse(answer);
+
+        string gradeLetter = "";
+
+        
     }
 }
