6b7e7cfaeb1fd8b372e86c38ba2095e8e671dd17
Kate Paletta <kate.f.knox@gmail.com>
2023-06-17 19:03:03 -0600

    created the guess while loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,6 +7,27 @@ class Program
         Random randomGenerator = new Random();
         int magicNum = randomGenerator.Next(1,101);
 
-        Console.Write($"The magic number is: {magicNum}");
+        // Console.Write($"The magic number is: {magicNum}");
+        int userNum = -1;
+
+        while (magicNum != userNum)
+        {
+            Console.Write("What is your guess?");
+            userNum = int.Parse(Console.ReadLine());
+
+            if (magicNum > userNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it! the magic number was {magicNum}");
+
+            }
+        }
     }
 }
