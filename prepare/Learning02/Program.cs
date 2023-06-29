78f4a154ec1fe4127f3306120777b674c881e388
Kate Paletta <kate.f.knox@gmail.com>
2023-06-29 12:00:53 -0600

    Resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Web Developer";
+        job1._company = "Microsoft";
+        job1._startYear = 2018;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Full Stack Developer";
+        job2._company = "Accumen";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        // job1.Display();
+        // job2.Display();
+
+        Resume resume1 = new Resume();
+        resume1._candidateName = "Kate Paletta";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
     }
 }
